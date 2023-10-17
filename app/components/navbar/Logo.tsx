daa793eaa21b3371698029a925ed1a59f37cb233
nuhptr <nugrahaadi733@gmail.com>
2023-10-17 21:13:42 +0700

    comp: navbar

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+
+export default function Logo() {
+   const router = useRouter()
+
+   return (
+      <Image
+         src='/airbnb-logo.png'
+         alt='Logo'
+         className='hidden cursor-pointer md:block'
+         height={100}
+         width={100}
+         onClick={() => router.push('/')}
+      />
+   )
+}
---
+++ app/components/navbar/UserMenu.tsx
@@ -0,0 +1,7 @@
+export default function UserMenu() {
+   return (
+      <div className="relative">
+         <div className="flex flex-row"></div>
+      </div>
+   )
+}
