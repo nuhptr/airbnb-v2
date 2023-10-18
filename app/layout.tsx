7ce73d3c2775a7b13e6571e0a863c1fa650b4005
nuhptr <nugrahaadi733@gmail.com>
2023-10-18 11:29:26 +0700

    update code layout and page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,27 @@
-import type { Metadata } from 'next'
-import { Nunito } from 'next/font/google'
+import type { Metadata } from "next"
+import { Nunito } from "next/font/google"
 
-import './globals.css'
+import "./globals.css"
 
-import Navbar from './components/navbar/Navbar'
+import Navbar from "./components/navbar/Navbar"
+import ClientOnly from "./components/ClientOnly"
+import RegisterModal from "./components/modals/RegisterModal"
 
-const font = Nunito({ subsets: ['latin'] })
+const font = Nunito({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-   title: 'Airbnb | place to stay in the world',
-   description: 'AirBnB website for booking rooms and houses in the world',
+   title: "Airbnb | place to stay in the world",
+   description: "AirBnB website for booking rooms and houses in the world",
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
    return (
-      <html lang='en'>
+      <html lang="en">
          <body className={font.className} suppressHydrationWarning={true}>
-            <Navbar />
+            <ClientOnly>
+               <RegisterModal />
+               <Navbar />
+            </ClientOnly>
             {children}
          </body>
       </html>
