4162f932dc868abfa2842f2459ebabdc0c2c9716
nuhptr <nugrahaadi733@gmail.com>
2023-10-18 12:49:21 +0700

    comp: update modal code

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -59,9 +59,7 @@ export default function Modal({
 
    return (
       <>
-         <div
-            onClick={onClose}
-            className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none bg-neutral-800/70">
+         <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none bg-neutral-800/70">
             <div className="relative w-full h-full mx-auto my-6 md:w-4/6 lg:w-3/6 xl:w-2/5 lg:h-auto md:h-auto">
                {/* CONTENT */}
                <div
