6f3084b94fc66484b206eb1a6bcb663a5077292c
nuhptr <nugrahaadi733@gmail.com>
2023-10-18 11:31:42 +0700

    comp: add button component

---
+++ app/components/Button.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { IconType } from "react-icons"
+
+type IButtonProps = {
+   label: string
+   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void
+   disabled?: boolean
+   outline?: boolean
+   small?: boolean
+   icon?: IconType
+}
+
+export default function Button({
+   label,
+   onClick,
+   disabled,
+   outline,
+   small,
+   icon: Icon,
+}: IButtonProps) {
+   return (
+      <button
+         onClick={onClick}
+         disabled={disabled}
+         className={`relative disabled:opacity-70 disabled:cursor-not-allowed rounded-lg hover:opacity-80 transition w-full
+         ${outline ? "bg-white border-black text-black" : "bg-rose-500 border-rose-500 text-white"}
+         ${small ? "py-1 text-sm font-light border-[1px]" : "py-3 text-md font-semibold border-2"}
+         `}>
+         {Icon && <Icon className="absolute left-4 top-3" size={24} />}
+         {label}
+      </button>
+   )
+}
